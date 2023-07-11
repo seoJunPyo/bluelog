c7fabd4243d51e49245c9b8eb290670288dd56ec
Jun Pyo Seo <cooljp95@naver.com>
2023-07-11 17:56:39 +0900

    [FEAT] 메인 검색 input 기능 추가

--- src/components/main/SearchInput.jsx
+++ src/components/main/SearchInput.jsx
@@ -1,22 +1,36 @@
 import React from 'react';
-import { Autocomplete } from '@mantine/core';
+import { TextInput } from '@mantine/core';
+import { useNavigate } from 'react-router';
 
-const SearchInput = () => (
-  <Autocomplete
-    placeholder="search keyword!"
-    data={[]}
-    radius="xl"
-    sx={{
-      padding: '16px',
-      input: {
-        height: '56px',
-        padding: '0 32px',
-        fontSize: '20px',
-        border: 'none',
-        boxShadow: 'var(--box-shadow)',
-      },
-    }}
-  />
-);
+const SearchInput = () => {
+  const inputRef = React.useRef(null);
+  const navigate = useNavigate();
+
+  const handleKeyUp = e => {
+    const value = inputRef.current.value.trim();
+    if (e.key !== 'Enter' || value === '') return;
+
+    navigate(`posts/search/${value}/1`);
+  };
+
+  return (
+    <TextInput
+      ref={inputRef}
+      placeholder="Search Keyword!"
+      radius="xl"
+      sx={{
+        padding: '16px',
+        input: {
+          height: '56px',
+          padding: '0 32px',
+          fontSize: '20px',
+          border: 'none',
+          boxShadow: 'var(--box-shadow)',
+        },
+      }}
+      onKeyUp={handleKeyUp}
+    />
+  );
+};
 
 export default SearchInput;
